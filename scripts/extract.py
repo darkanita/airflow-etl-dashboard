7086ccb8ba63fd62993d0f068feb78e231eeb7f2
Ana María López Moreno <darkanita@gmail.com>
2026-03-30 20:31:55 -0500

    Update extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -5,6 +5,6 @@ def extract_data():
     url = "https://people.sc.fsu.edu/~jburkardt/data/csv/airtravel.csv"
     df = pd.read_csv(url)
     
-    os.makedirs('/opt/airflow/dags/data', exist_ok=True)
-    df.to_csv('/opt/airflow/dags/data/airtravel_raw.csv', index=False)
+    os.makedirs('/usr/local/airflow/dags/data', exist_ok=True)
+    df.to_csv('/usr/local/airflow/dags/data/airtravel_raw.csv', index=False)
     print("✅ CSV descargado y guardado.")
