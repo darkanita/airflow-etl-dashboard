0012c8998cf960fc89edf803007ba632b1e3e891
Ana María López Moreno <darkanita@gmail.com>
2026-03-30 20:32:12 -0500

    Update database paths in load.py

--- scripts/load.py
+++ scripts/load.py
@@ -3,10 +3,10 @@ import pandas as pd
 import os
 
 def load_data():
-    db_path = '/opt/airflow/dags/data/airtravel.db'
+    db_path = '/usr/local/airflow/dags/data/airtravel.db'
     conn = sqlite3.connect(db_path)
 
-    df = pd.read_csv('/opt/airflow/dags/data/airtravel_transformed.csv')
+    df = pd.read_csv('/usr/local/airflow/dags/data/airtravel_transformed.csv')
     df.to_sql('airtravel_data', conn, if_exists='replace', index=False)
     conn.close()
     print("✅ Datos cargados en SQLite.")
