216374db46ed09c67d57bdf30342fa5813754527
Ana María López Moreno <darkanita@gmail.com>
2026-03-30 20:32:31 -0500

    Update file paths for data transformation

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def transform_data():
-    df = pd.read_csv('/opt/airflow/dags/data/airtravel_raw.csv')
+    df = pd.read_csv('/usr/local/airflow/dags/data/airtravel_raw.csv')
     df_long = df.melt(id_vars=["Month"], var_name="Year", value_name="Passengers")
-    df_long.to_csv('/opt/airflow/dags/data/airtravel_transformed.csv', index=False)
+    df_long.to_csv('/usr/local/airflow/dags/data/airtravel_transformed.csv', index=False)
     print("✅ Datos transformados y guardados.")
